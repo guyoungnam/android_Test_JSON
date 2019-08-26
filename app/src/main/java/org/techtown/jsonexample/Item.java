1885438cd23ceb7c39db3e1dac02fa00780681eb
guyoungnam <eak12@naver.com>
2019-08-26 17:02:11 +0900

    "JSON 데이터 이동 "

---
+++ app/src/main/java/org/techtown/jsonexample/Item.java
@@ -0,0 +1,34 @@
+package org.techtown.jsonexample;
+
+public class Item {
+
+    private String nation;
+    private String name;
+    private String address;
+    private String age;
+
+    public Item(String nation, String name, String address, String age) {
+        this.nation = nation;
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+
+}
